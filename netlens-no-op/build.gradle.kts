bc4ce7594bb15e094b0f9dd9b7467d88d4a2e11e
Muhammad Hamza Shahid <64645297+hamza863@users.noreply.github.com>
2026-05-22 16:29:06 +0500

    Bump version to 1.0.6 in build.gradle.kts

--- netlens-no-op/build.gradle.kts
+++ netlens-no-op/build.gradle.kts
@@ -27,7 +27,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId    = "com.github.hamza863"
                 artifactId = "netlens-no-op"
-                version    = "1.0.5"
+                version    = "1.0.6"
             }
         }
     }
